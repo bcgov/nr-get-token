9dbd18f2d7a63ca43789432fbd000295829fe26e
Jeremy Ho <jujaga@gmail.com>
2019-04-10 13:30:34 -0700

    Add work in progress form field
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- src/components/GetToken.vue
+++ src/components/GetToken.vue
@@ -1,12 +1,23 @@
 <template>
-  <h1>Hello World</h1>
+  <v-layout>
+    <v-flex xs12 md6>
+      <v-text-field
+        v-model="firstname"
+        :rules="nameRules"
+        :counter="10"
+        label="First name"
+        required
+      ></v-text-field>
+    </v-flex>
+
+    <v-flex xs12 md6>
+      <v-text-field v-model="lastname" :rules="nameRules" :counter="10" label="Last name" required></v-text-field>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
 </script>
 
 <style>
-.v-card--flat {
-  background-color: transparent !important;
-}
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,98 +1,86 @@
 <template>
   <v-container>
-    <v-layout
-      text-xs-center
-      wrap
-    >
+    <v-layout text-xs-center wrap>
       <v-flex xs12>
-        <v-img
-          :src="require('@/assets/images/logo.svg')"
-          class="my-3"
-          contain
-          height="100"
-        ></v-img>
+        <v-img :src="require('@/assets/images/logo.svg')" class="my-3" contain height="100"></v-img>
       </v-flex>
 
       <v-flex mb-4>
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Natural Resources Get Token
-        </h1>
+        <h1 class="display-2 font-weight-bold mb-3">Welcome to Natural Resources Get Token</h1>
       </v-flex>
-      
+    </v-layout>
+
+    <v-layout text-xs-center wrap>
       <v-flex>
         <GetToken></GetToken>
       </v-flex>
-
     </v-layout>
   </v-container>
 </template>
 
 <script>
-  import GetToken from './GetToken';
-  
-  export default {
+import GetToken from "./GetToken";
 
-    name: 'home',
-    components: {
-      GetToken
-    },
+export default {
+  name: "home",
+  components: {
+    GetToken
+  },
 
-    data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader'
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify'
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify'
-        }
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com'
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com'
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuetifyjs.com'
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs'
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify'
-        }
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer'
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/layout/pre-defined'
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
-        }
-
-      ]
-    })
-  }
+  data: () => ({
+    ecosystem: [
+      {
+        text: "vuetify-loader",
+        href: "https://github.com/vuetifyjs/vuetify-loader"
+      },
+      {
+        text: "github",
+        href: "https://github.com/vuetifyjs/vuetify"
+      },
+      {
+        text: "awesome-vuetify",
+        href: "https://github.com/vuetifyjs/awesome-vuetify"
+      }
+    ],
+    importantLinks: [
+      {
+        text: "Documentation",
+        href: "https://vuetifyjs.com"
+      },
+      {
+        text: "Chat",
+        href: "https://community.vuetifyjs.com"
+      },
+      {
+        text: "Made with Vuetify",
+        href: "https://madewithvuetifyjs.com"
+      },
+      {
+        text: "Twitter",
+        href: "https://twitter.com/vuetifyjs"
+      },
+      {
+        text: "Articles",
+        href: "https://medium.com/vuetify"
+      }
+    ],
+    whatsNext: [
+      {
+        text: "Explore components",
+        href: "https://vuetifyjs.com/components/api-explorer"
+      },
+      {
+        text: "Select a layout",
+        href: "https://vuetifyjs.com/layout/pre-defined"
+      },
+      {
+        text: "Frequently Asked Questions",
+        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions"
+      }
+    ]
+  })
+};
 </script>
 
 <style>
-
 </style>
