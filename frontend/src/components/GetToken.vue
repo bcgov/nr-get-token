d392b9ccf953ab325789348107c050ab19e3acdc
lucas <lucas.oneil@gov.bc.ca>
2019-04-15 14:40:26 -0700

    Initial version of Get Token and Post Config flow

--- frontend/src/components/GetToken.vue
+++ frontend/src/components/GetToken.vue
@@ -1,23 +1,85 @@
 <template>
-  <v-layout>
-    <v-flex xs12 md6>
-      <v-text-field
-        v-model="firstname"
-        :rules="nameRules"
-        :counter="10"
-        label="First name"
-        required
-      ></v-text-field>
-    </v-flex>
+  <v-form ref="form" v-model="valid">
+    <v-container>
+      <v-layout>
+        <v-flex xs12 md5>
+          <v-text-field
+            v-model="serviceClient"
+            label="Service Client"
+            value="GETOK_SERVICE"
+            readonly
+            required
+          ></v-text-field>
 
-    <v-flex xs12 md6>
-      <v-text-field v-model="lastname" :rules="nameRules" :counter="10" label="Last name" required></v-text-field>
-    </v-flex>
-  </v-layout>
+          <v-text-field v-model="password" type="password" label="Password" required></v-text-field>
+
+          <v-btn :disabled="!valid" color="success" @click="handleSubmit">Submit</v-btn>
+        </v-flex>
+        <v-flex xs12 md6 offset-md1>
+          <v-textarea
+            id="tokenResponse"
+            rows="10"
+            placeholder="This field will be filled by the token"
+            auto-grow
+            readonly
+            label="Token Response"
+          ></v-textarea>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-form>
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      serviceClient: "GETOK_SERVICE",
+      password: "",
+      tokenResponse: ""
+    };
+  },
+
+  methods: {
+    handleSubmit() {
+      console.log("submitted");
+      const url = `https://i1api.nrs.gov.bc.ca/oauth2/v1/oauth/token?disableDeveloperFilter=true&grant_type=client_credentials&scope=WEBADE-REST.*`;
+
+      const headers = new Headers();
+      headers.set(
+        "Authorization",
+        "Basic " + window.btoa("GETOK_SERVICE" + ":" + this.password)
+      );
+
+      fetch(url, {
+        method: "get",
+        headers: headers
+      })
+        .then(resp => resp.json())
+        .then(function(data) {
+          // TODO: This is all hard JS. Should be moved to proper Vue
+          console.log(data);
+          const tokenResponseField = document.querySelector("#tokenResponse");
+          const hiddenTokenField = document.querySelector("#hiddenToken");
+          tokenResponseField.value = JSON.stringify(data, null, 2);
+          if (data.access_token) {
+            hiddenTokenField.value = data.access_token;
+            const appSubmit = document.querySelector("#submitAppConfig");
+            appSubmit.disabled = false;
+            appSubmit.classList.remove("v-btn--disabled", "success--text");
+            appSubmit.classList.add("success");
+          }
+        })
+        .catch(function(error) {
+          console.log(`ERROR, caught error fetching from ${url}`);
+          console.log(error);
+        });
+    }
+  }
+};
 </script>
+
+
 
 <style>
 </style>
--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -2,17 +2,48 @@
   <v-container>
     <v-layout text-xs-center wrap>
       <v-flex xs12>
-        <v-img :src="require('@/assets/images/logo.svg')" class="my-3" contain height="100"></v-img>
-      </v-flex>
-
-      <v-flex mb-4>
-        <h1 class="display-2 font-weight-bold mb-3">Welcome to Natural Resources Get Token</h1>
+        <v-img :src="require('@/assets/images/Get-Token_md.png')" class="my-3" contain height="150"></v-img>
       </v-flex>
     </v-layout>
 
-    <v-layout text-xs-center wrap>
-      <v-flex>
-        <GetToken></GetToken>
+    <v-layout row wrap>
+      <v-flex xs112>
+        <v-card class="sectionCard">
+          <v-toolbar card color="grey lighten-3">
+            <v-toolbar-title>Fetch GETOK Token</v-toolbar-title>
+          </v-toolbar>
+          <GetToken></GetToken>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12>
+        <v-card class="sectionCard">
+          <v-toolbar card color="grey lighten-3">
+            <v-toolbar-title>Submit Application Configuration</v-toolbar-title>
+          </v-toolbar>
+          <v-container>
+            <v-layout>
+              <v-flex xs12>
+                <v-form>
+                  <v-textarea
+                    rows="1"
+                    placeholder="Enter configuration JSON here"
+                    auto-grow
+                    label="WebADE Application Config"
+                    v-model="appConfig"
+                  ></v-textarea>
+                  <v-btn
+                    id="submitAppConfig"
+                    disabled
+                    color="success"
+                    @click="handleSubmitAppConfig"
+                  >Submit</v-btn>
+                  <input type="hidden" id="hiddenToken">
+                </v-form>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
@@ -26,61 +57,46 @@ export default {
   components: {
     GetToken
   },
+  data() {
+    return {
+      hiddenToken: "",
+      appConfig: ""
+    };
+  },
 
-  data: () => ({
-    ecosystem: [
-      {
-        text: "vuetify-loader",
-        href: "https://github.com/vuetifyjs/vuetify-loader"
-      },
-      {
-        text: "github",
-        href: "https://github.com/vuetifyjs/vuetify"
-      },
-      {
-        text: "awesome-vuetify",
-        href: "https://github.com/vuetifyjs/awesome-vuetify"
-      }
-    ],
-    importantLinks: [
-      {
-        text: "Documentation",
-        href: "https://vuetifyjs.com"
-      },
-      {
-        text: "Chat",
-        href: "https://community.vuetifyjs.com"
-      },
-      {
-        text: "Made with Vuetify",
-        href: "https://madewithvuetifyjs.com"
-      },
-      {
-        text: "Twitter",
-        href: "https://twitter.com/vuetifyjs"
-      },
-      {
-        text: "Articles",
-        href: "https://medium.com/vuetify"
-      }
-    ],
-    whatsNext: [
-      {
-        text: "Explore components",
-        href: "https://vuetifyjs.com/components/api-explorer"
-      },
-      {
-        text: "Select a layout",
-        href: "https://vuetifyjs.com/layout/pre-defined"
-      },
-      {
-        text: "Frequently Asked Questions",
-        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions"
-      }
-    ]
-  })
+  methods: {
+    handleSubmitAppConfig() {
+      const token = document.querySelector("#hiddenToken").value;
+
+      const url = `https://i1api.nrs.gov.bc.ca/webade-api/v1/applicationConfigurations`;
+
+      const headers = new Headers();
+      headers.set("Authorization", `Bearer ${token}`);
+      headers.set("Content-Type", "application/json");
+
+      const config = JSON.parse(this.appConfig);
+
+      fetch(url, {
+        method: "POST",
+        body: this.appConfig,
+        headers: headers
+      })
+        .then(res => res.json())
+        .then(function(response) {
+          console.log("Success:", JSON.stringify(response));
+          alert(`SUCCESS, application configuration for ${config.applicationAcronym} updated in Integration`)
+        })
+        .catch(function(error) {
+          console.error("Error:", error);
+          alert("ERROR, see console");
+        });
+    }
+  }
 };
 </script>
 
 <style>
+.sectionCard {
+  margin-bottom: 20px;
+}
 </style>
