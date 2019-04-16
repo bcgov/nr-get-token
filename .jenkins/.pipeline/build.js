490e514967d74a3edf7a73309160170a517f942b
Jason Sherman <jsherman@parcsystems.ca>
2019-04-16 13:53:15 -0700

    add jenkins setup with pr and master job
    tools/jenkins setup instructions.  will install jenkins + slave, configured with pr job and master job.

--- .jenkins/.pipeline/build.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-const phases = require('./lib/config')
-const build = require('./lib/build.js')
-
-build({phases:phases})
--- .jenkins/.pipeline/deploy.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-const deploy = require('./lib/deploy.js')
-deploy()
