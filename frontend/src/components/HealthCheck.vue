3b128c8789a72ebb9d701520dbb710b03848647c
Lucas O'Neil <lucasoneil@gmail.com>
2019-05-17 17:40:56 -0700

    remove debug data

--- frontend/src/components/HealthCheck.vue
+++ frontend/src/components/HealthCheck.vue
@@ -41,36 +41,6 @@
 import { mapState } from 'vuex';
 
 export default {
-  data() {
-    return {
-      items: [
-        {
-          active: true,
-          title: 'Jason Oner',
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg'
-        },
-        {
-          active: true,
-          title: 'Ranee Carlson',
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg'
-        },
-        {
-          title: 'Cindy Baker',
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg'
-        },
-        {
-          title: 'Ali Connors',
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg'
-        }
-      ],
-      items2: [
-        {
-          title: 'Travis Howard',
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg'
-        }
-      ]
-    };
-  },
   computed: mapState(['healthCheck']),
   methods: {
     getStatus(status) {
