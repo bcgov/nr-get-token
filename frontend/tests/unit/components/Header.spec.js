3a395e609e531b1c77dd294b84e4399d31fefbd9
Jeremy Ho <jujaga@gmail.com>
2019-05-27 16:36:51 -0700

    Add missing vuex store mocking Disable build cleanup in Jenkins
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- frontend/tests/unit/components/Header.spec.js
+++ frontend/tests/unit/components/Header.spec.js
@@ -1,17 +1,30 @@
 import { mount, createLocalVue } from '@vue/test-utils';
 import Vuetify from 'vuetify';
+import Vuex from 'vuex';
 import Header from '@/components/Header.vue';
 
 describe('Header.vue', () => {
+  let getters;
+  let store;
   let wrapper;
 
   beforeEach(() => {
     const localVue = createLocalVue();
 
     localVue.use(Vuetify);
+    localVue.use(Vuex);
+
+    getters = {
+      isAuthenticated: () => 'false'
+    };
+
+    store = new Vuex.Store({
+      getters
+    });
 
     wrapper = mount(Header, {
-      localVue: localVue
+      localVue,
+      store
     });
   });
 
