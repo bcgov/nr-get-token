f67304a673c5873b1e407f77840d345ae66ba19f
Jeremy Ho <jujaga@gmail.com>
2019-05-29 15:56:55 -0700

    Fix Vue proxy config to handle multiple api paths
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,11 +3,19 @@ module.exports = {
   // To override, see .env.development file
   devServer: {
     proxy: {
-      '^/getok/api': {
-        target: process.env.VUE_APP_API_ROOT,
-        changeOrigin: true,
-        ws: false
-      }
+      // Using reduce syntax to represent multiple mount points
+      // https://github.com/vuejs/vue-cli/issues/2285#issuecomment-462061125
+      ...['/api', '/getok/api'].reduce(
+        (acc, ctx) => ({
+          ...acc,
+          [ctx]: {
+            target: process.env.VUE_APP_API_ROOT,
+            changeOrigin: true,
+            ws: false
+          }
+        }),
+        {}
+      ),
     }
   },
   publicPath: process.env.NODE_ENV === 'production'
