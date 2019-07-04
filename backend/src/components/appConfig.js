8ea44cff49db8918478bbfcb71bab4c366205ab8
Jeremy Ho <jujaga@gmail.com>
2019-07-04 15:57:49 -0700

    Move buildWebAdeCfg from uitls to appConfig component
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- backend/src/components/appConfig.js
+++ backend/src/components/appConfig.js
@@ -4,6 +4,123 @@ const log = require('npmlog');
 
 const utils = require('./utils');
 
+// Constructs a WebADE Application Configuration based on the request body
+// TODO: Refactor this to not take a response body -> instead taking in explicit variable parameters
+function buildWebAdeCfg(requestBody) {
+  const configForm = requestBody.configForm;
+  const generatedPassword = utils.generateEncryptPassword(requestBody.passwordPublicKey);
+  const defaultAppCfg = {
+    '@type': 'http://webade.gov.bc.ca/applicationConfiguration',
+    applicationAcronym: '',
+    custodianNumber: 0,
+    applicationName: '',
+    applicationDescription: '',
+    applicationObjectPrefix: null,
+    enabledInd: true,
+    distributeTypeCd: null,
+    managementEnabledInd: false,
+    applicationVersion: null,
+    reportedWebadeVersion: null,
+    actions: [],
+    roles: [],
+    wdePreferences: [],
+    applicationPreferences: [],
+    globalPreferences: [],
+    defaultUserPreferences: [],
+    profiles: [],
+    serviceClients: [],
+    groupAuthorizations: []
+  };
+
+  // Set up the conditional JSON structure based on user entry
+  const newAppCfg = {
+    applicationAcronym: configForm.applicationAcronym,
+    applicationName: configForm.applicationName,
+    applicationDescription: configForm.applicationDescription
+  };
+
+  if (!configForm.applicationAcronym) {
+    newAppCfg.serviceClients = [];
+  } else {
+    newAppCfg.serviceClients = [{
+      accountName: `${newAppCfg.applicationAcronym}_SERVICE_CLIENT`,
+      secret: '',
+      oauthScopes: [],
+      oauthGrantTypes: [],
+      oauthRedirectUrls: [],
+      oauthAccessTokenValidity: null,
+      oauthRefreshTokenValidity: null,
+      oauthAdditionalInformation: '{"autoapprove":"true"}',
+      authorizations: []
+    }];
+
+    // Set up password
+    newAppCfg.serviceClients[0].secret = generatedPassword.password;
+
+    if (!configForm.commonServices || !configForm.commonServices.length) {
+      newAppCfg.serviceClients[0].authorizations = [];
+    } else {
+      newAppCfg.actions = [
+        {
+          name: `${newAppCfg.applicationAcronym}_ACTION`,
+          description: `${newAppCfg.applicationAcronym} action`,
+          privilegedInd: false
+        }
+      ];
+
+      newAppCfg.roles = [
+        {
+          name: `${newAppCfg.applicationAcronym}_ROLE`,
+          description: `${newAppCfg.applicationAcronym} Role`,
+          actionNames: [
+            `${newAppCfg.applicationAcronym}_ACTION`
+          ]
+        }
+      ];
+
+      newAppCfg.profiles = [
+        {
+          name: `${newAppCfg.applicationAcronym}_PROFILE`,
+          description: `Can send an email with the ${newAppCfg.applicationAcronym} app`,
+          secureByOrganization: false,
+          availibleTo: [
+            'SCL'
+          ],
+          effectiveDate: 1506582000000,
+          expiryDate: 253402243200000,
+          profileRoles: [
+            {
+              applicationCode: newAppCfg.applicationAcronym,
+              name: `${newAppCfg.applicationAcronym}_ROLE`
+            },
+            {
+              applicationCode: 'CMSG',
+              name: 'SENDER'
+            }
+          ]
+        }
+      ];
+
+      newAppCfg.serviceClients[0].authorizations = [{
+        profileName: `${newAppCfg.applicationAcronym}_PROFILE`,
+        profileDescription: 'Test profile description',
+        effectiveDate: 1506629523000,
+        expiryDate: 253402243200000,
+        enabled: true
+      }];
+    }
+  }
+
+  const finalCfg = { ...defaultAppCfg, ...newAppCfg };
+
+  return {
+    webAdeCfg: finalCfg,
+    unencryptedPassword: generatedPassword.password,
+    encryptedPassword: generatedPassword.encryptedPassword
+  };
+}
+
+// TODO: Refactor this to not take a response body -> instead taking in explicit variable parameters
 async function postAppConfig(body) {
   const webadeEnv = body.configForm.webadeEnvironment;
   const endpoint = config.get(`serviceClient.getok${utils.toPascalCase(webadeEnv)}.endpoint`);
@@ -17,7 +134,7 @@ async function postAppConfig(body) {
   }
 
   // Build the app config
-  const generatedConfig = utils.buildWebAdeCfg(body);
+  const generatedConfig = buildWebAdeCfg(body);
 
   // Submit the app config to webade
   const path = '/applicationConfigurations';
@@ -33,7 +150,7 @@ async function postAppConfig(body) {
 
     const reponse = {
       webAdeResponse: webAdeResponse.data,
-      generatedPassword: generatedConfig.webAdeCfg.serviceClients ? generatedConfig.encyptedPassword : '',
+      generatedPassword: generatedConfig.webAdeCfg.serviceClients ? generatedConfig.encryptedPassword : '',
       generatedServiceClient: generatedConfig.webAdeCfg.serviceClients ? generatedConfig.webAdeCfg.serviceClients[0].accountName : ''
     };
     return reponse;
--- backend/src/components/utils.js
+++ backend/src/components/utils.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
 const config = require('config');
-const log = require('npmlog');
 const cryptico = require('cryptico-js');
 const generator = require('generate-password');
+const log = require('npmlog');
 
 let discovery = null;
 
@@ -48,122 +48,6 @@ const utils = {
     }
 
     return discovery;
-  },
-
-  // Constructs a WebADE Application Configuration based on the request body
-  buildWebAdeCfg: requestBody => {
-    const configForm = requestBody.configForm;
-    const generatedPassword = utils.generateEncryptPassword(requestBody.passwordPublicKey);
-    const defaultAppCfg = {
-      '@type': 'http://webade.gov.bc.ca/applicationConfiguration',
-      applicationAcronym: '',
-      custodianNumber: 0,
-      applicationName: '',
-      applicationDescription: '',
-      applicationObjectPrefix: null,
-      enabledInd: true,
-      distributeTypeCd: null,
-      managementEnabledInd: false,
-      applicationVersion: null,
-      reportedWebadeVersion: null,
-      actions: [],
-      roles: [],
-      wdePreferences: [],
-      applicationPreferences: [],
-      globalPreferences: [],
-      defaultUserPreferences: [],
-      profiles: [],
-      serviceClients: [],
-      groupAuthorizations: []
-    };
-
-    // Set up the conditional JSON structure based on user entry
-    const newAppCfg = {
-      applicationAcronym: configForm.applicationAcronym,
-      applicationName: configForm.applicationName,
-      applicationDescription: configForm.applicationDescription
-    };
-
-    if (!configForm.applicationAcronym) {
-      newAppCfg.serviceClients = [];
-    } else {
-      newAppCfg.serviceClients = [{
-        accountName: `${newAppCfg.applicationAcronym}_SERVICE_CLIENT`,
-        secret: '',
-        oauthScopes: [],
-        oauthGrantTypes: [],
-        oauthRedirectUrls: [],
-        oauthAccessTokenValidity: null,
-        oauthRefreshTokenValidity: null,
-        oauthAdditionalInformation: '{"autoapprove":"true"}',
-        authorizations: []
-      }];
-
-      // Set up password
-      newAppCfg.serviceClients[0].secret = generatedPassword.password;
-
-      if (!configForm.commonServices || !configForm.commonServices.length) {
-        newAppCfg.serviceClients[0].authorizations = [];
-      } else {
-        newAppCfg.actions = [
-          {
-            name: `${newAppCfg.applicationAcronym}_ACTION`,
-            description: `${newAppCfg.applicationAcronym} action`,
-            privilegedInd: false
-          }
-        ];
-
-        newAppCfg.roles = [
-          {
-            name: `${newAppCfg.applicationAcronym}_ROLE`,
-            description: `${newAppCfg.applicationAcronym} Role`,
-            actionNames: [
-              `${newAppCfg.applicationAcronym}_ACTION`
-            ]
-          }
-        ];
-
-        newAppCfg.profiles = [
-          {
-            name: `${newAppCfg.applicationAcronym}_PROFILE`,
-            description: `Can send an email with the ${newAppCfg.applicationAcronym} app`,
-            secureByOrganization: false,
-            availibleTo: [
-              'SCL'
-            ],
-            effectiveDate: 1506582000000,
-            expiryDate: 253402243200000,
-            profileRoles: [
-              {
-                applicationCode: newAppCfg.applicationAcronym,
-                name: `${newAppCfg.applicationAcronym}_ROLE`
-              },
-              {
-                applicationCode: 'CMSG',
-                name: 'SENDER'
-              }
-            ]
-          }
-        ];
-
-        newAppCfg.serviceClients[0].authorizations = [{
-          profileName: `${newAppCfg.applicationAcronym}_PROFILE`,
-          profileDescription: 'Test profile description',
-          effectiveDate: 1506629523000,
-          expiryDate: 253402243200000,
-          enabled: true
-        }];
-      }
-    }
-
-    const finalCfg = { ...defaultAppCfg, ...newAppCfg };
-
-    const ret = {
-      webAdeCfg: finalCfg,
-      unencryptedPassword: generatedPassword.password,
-      encryptedPassword: generatedPassword.encryptedPassword
-    };
-    return ret;
   },
 
   // Creates a random password of a certain length and encrypts it with the key
