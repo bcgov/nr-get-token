51d20eea2e3323905a1bb4535c0d0dc372233310
Jeremy Ho <jujaga@gmail.com>
2019-07-10 16:17:45 -0700

    Add branch coverage for 200 response on refresh endpoint
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- backend/tests/unit/routes/auth.spec.js
+++ backend/tests/unit/routes/auth.spec.js
@@ -1,6 +1,10 @@
 const request = require('supertest');
 
+const auth = require('../../../src/components/auth');
 const app = require('../../../app');
+
+const endlessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOjB9.JWKPB-5Q8rTYzl-MfhRGpP9WpDpQxC7JkIAGFMDZnpg';
+const validToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOjk5OTk5OTk5OTl9.Vg30C57s3l90JNap_VgMhKZjfc-p7SoBXaSAy8c28HA';
 
 describe('/api/auth', () => {
   it('should return all available endpoints', async () => {
@@ -55,7 +59,23 @@ describe('/api/auth/logout', () => {
 });
 
 describe('/api/auth/refresh', () => {
-  it('should have a response', async () => {
+  it('should have a 200 response', async () => {
+    auth.renew = jest.fn().mockResolvedValue({
+      access_token: validToken,
+      refresh_token: endlessToken
+    });
+
+    const response = await request(app)
+      .post('/api/auth/refresh')
+      .send({
+        refreshToken: endlessToken
+      });
+
+    expect(response).toBeTruthy();
+    expect(response.statusCode).toBe(200);
+  });
+
+  it('should have a 400 response', async () => {
     const response = await request(app).post('/api/auth/refresh');
     expect(response).toBeTruthy();
     expect(response.statusCode).toBe(400);
