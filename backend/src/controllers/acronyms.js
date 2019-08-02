f716dd7ef6d135dd01dd2fabc9ec9e3537834335
Jeremy Ho <jujaga@gmail.com>
2019-08-02 16:23:26 -0700

    Add initial acronyms controller
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- backend/src/controllers/acronyms.js
+++ backend/src/controllers/acronyms.js
@@ -1,3 +1,32 @@
-// const Acronym = require('../models').Acronym;
+const Acronym = require('../models').Acronym;
 
-module.exports = {};
+module.exports = {
+  async find(acronym) {
+    return await Acronym.findOne({
+      where: {
+        acronym: acronym
+      }
+    });
+  },
+
+  async findOrCreate(acronym, name) {
+    return await Acronym.findOrCreate({
+      where: {
+        acronym: acronym
+      },
+      defaults: {
+        name: name
+      }
+    });
+  },
+
+  async updateName(acronym, name) {
+    return await Acronym.update({
+      name: name
+    }, {
+      where: {
+        acronym: acronym
+      }
+    });
+  }
+};
