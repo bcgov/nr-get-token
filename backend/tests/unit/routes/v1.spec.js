7893efd66191bf086277bf45e37b83444c9ec050
Lucas O'Neil <lucasoneil@gmail.com>
2019-10-18 16:40:09 -0700

    Proposed spec changes

--- backend/tests/unit/routes/v1.spec.js
+++ backend/tests/unit/routes/v1.spec.js
@@ -33,7 +33,7 @@ describe('/api/v1/api-spec.yaml', () => {
     const response = await request(app).get('/api/v1/api-spec.yaml');
 
     expect(response.statusCode).toBe(200);
-    expect(response.text).toMatch(/openapi: 3.0.0/);
+    expect(response.text).toMatch(/openapi: 3.0.2/);
     expect(response.text).toMatch(/title: GetOK API/);
   });
 });
