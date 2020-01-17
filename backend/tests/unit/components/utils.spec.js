f73c7bbb385af53be987481113bbc07e624e4d4c
Lucas O'Neil <lucasoneil@gmail.com>
2020-01-17 14:47:12 -0800

    New util function for kc token fetch

--- backend/tests/unit/components/utils.spec.js
+++ backend/tests/unit/components/utils.spec.js
@@ -78,6 +78,66 @@ describe('getWebAdeToken', () => {
   });
 });
 
+describe('getKeyCloakToken', () => {
+  const endpoint = config.get('serviceClient.ches.tokenEndpoint');
+  const username = config.get('serviceClient.ches.username');
+  const password = config.get('serviceClient.ches.password');
+
+  const spy = jest.spyOn(axios, 'post');
+
+  afterEach(() => {
+    spy.mockClear();
+  });
+
+  it('should call KC endpoint to get a token', async () => {
+    mockAxios.onPost().reply(200, {
+      data: {
+        'access_token': 'hsdfs79fsdiufhew89ijsdf9.dshufu9dshf98dsfhsdf.89sdhfs8d9hfuh',
+        'expires_in': 300,
+        'refresh_expires_in': 1800,
+        'refresh_token': 'sdklfjkdskjf87.sdiohfsduh8.sdfioj898',
+        'token_type': 'bearer',
+        'not-before-policy': 0,
+        'session_state': 'f4464085-3cbc-498b-96d3-0a33837d2ae8',
+        'scope': ''
+      }
+    });
+
+    const result = await utils.getKeyCloakToken(username, password, endpoint);
+
+    const params = new URLSearchParams();
+    params.append('grant_type', 'client_credentials');
+
+    expect(result.data).toBeTruthy();
+    expect(result.data.access_token).toEqual('hsdfs79fsdiufhew89ijsdf9.dshufu9dshf98dsfhsdf.89sdhfs8d9hfuh');
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith({
+      method: 'POST',
+      auth: {
+        username: username,
+        password: password
+      },
+      headers: { 'content-type': 'application/x-www-form-urlencoded' },
+      data: params,
+      url: endpoint,
+    });
+  });
+
+  it('should gracefully fail if endpoint is down', async () => {
+    mockAxios.onPost().reply(400, {
+      data: {
+        'error': 'invalid_request',
+        'error_description': 'Invalid grant_type'
+      }
+    });
+
+    const result = await utils.getKeyCloakToken(username, password, endpoint);
+
+    expect(result).toBeTruthy();
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+});
+
 describe('getOidcDiscovery', () => {
   const url = config.get('oidc.discovery');
   const spy = jest.spyOn(axios, 'get');
