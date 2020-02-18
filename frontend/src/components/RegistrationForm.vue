8d3a8555afb16233d15b09d0d15a40d293b07a3c
Csaky <Tim.Csaky@gov.bc.ca>
2020-02-18 12:24:42 -0800

    styled registration form to match Mina's concept

--- frontend/src/components/RegistrationForm.vue
+++ frontend/src/components/RegistrationForm.vue
@@ -1,69 +1,107 @@
 <template>
   <v-btn class="mr-2" color="success" @click="registrationDialog = true">
-    Register a new app
-    <v-dialog v-model="registrationDialog" persistent max-width="700">
+    Register a new application
+    <v-dialog v-model="registrationDialog" persistent max-width="500">
       <v-card>
-        <v-card-title class="headline">
-          <v-icon class="mr-2">email</v-icon>Register a New Application
+        <v-card-title class="headline underline">
+          <v-icon class="mr-2">email</v-icon>Request Permission
         </v-card-title>
         <v-card-text>
           <p>
-            Please enter an acronym for the application you are registering.
-            <br />Use the optional Comments field to describe your project or indicate relavant branch / ministries.
-            <br />This message is sent to the Common Service Showcase Team to authorize you for that application.
+            Please submit an Acronym for the application you wish to add.
+            <br />You will get an email once permissions have been set up.
           </p>
           <v-form ref="form" v-model="valid" lazy-validation>
-            <p class="mb-0">
-              My Email:
-              <strong>{{userInfo.emailAddress}}</strong>
-              <br />My IDIR:
-              <strong>{{userInfo.idir}}</strong>
-            </p>
             <v-row>
-              <v-col cols="12" sm="6">
+              <v-col>
+                <label>My Email</label>
                 <v-text-field
-                  :counter="fieldValidations.ACRONYM_MAX_LENGTH"
-                  label="Application Acronym"
+                  readonly
+                  :value="userInfo.emailAddress"
+                  hide-details="auto"
+                  solo
+                  dense
+                  single-line
+                  outlined
+                  flat
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col>
+                <label>My IDIR</label>
+                <v-text-field
+                  readonly
+                  :value="userInfo.idir"
+                  hide-details="auto"
+                  solo
+                  single-line
+                  dense
+                  outlined
+                  flat
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col>
+                <label>Application Acronym &nbsp;</label>
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on }">
+                    <v-icon v-on="on">help_outline</v-icon>
+                  </template>
+                  The Application Acronym must comply with the following format:
+                  <ul>
+                    <li>UPPERCASE LETTERS ONLY</li>
+                    <li>Underscores may be placed between letters</li>
+                    <li>Must begin and end with a letter</li>
+                    <li>At least {{ fieldValidations.ACRONYM_MIN_LENGTH }} characters</li>
+                    <li>
+                      Examples:
+                      <em>ABCD</em>,
+                      <em>ABCD_WXYZ</em>
+                    </li>
+                  </ul>
+                </v-tooltip>
+                <v-text-field
+                  placeholder="For example: 'ABC_DEF'"
                   required
                   :rules="applicationAcronymRules"
                   v-model="applicationAcronym"
+                  hide-details="auto"
+                  solo
+                  dense
+                  single-line
+                  outlined
+                  flat
                 >
-                  <template v-slot:append-outer>
-                    <v-tooltip bottom>
-                      <template v-slot:activator="{ on }">
-                        <v-icon v-on="on">help_outline</v-icon>
-                      </template>
-                      The Application Acronym must comply with the following format:
-                      <ul>
-                        <li>UPPERCASE LETTERS ONLY</li>
-                        <li>Underscores may be placed between letters</li>
-                        <li>Must begin and end with a letter</li>
-                        <li>At least {{ fieldValidations.ACRONYM_MIN_LENGTH }} characters</li>
-                        <li>
-                          Examples:
-                          <em>ABCD</em>,
-                          <em>ABCD_WXYZ</em>
-                        </li>
-                      </ul>
-                    </v-tooltip>
-                  </template>
+                  <template v-slot:append-outer></template>
                 </v-text-field>
               </v-col>
             </v-row>
-
-            <v-textarea v-model="comments" rows="1" auto-grow label="Comments"></v-textarea>
+            <v-row>
+              <v-col>
+                <label>Comments (optional)</label>
+                <v-textarea
+                  v-model="comments"
+                  rows="3"
+                  auto-grow
+                  hide-details="auto"
+                  dense
+                  solo
+                  outlined
+                  flat
+                ></v-textarea>
+              </v-col>
+            </v-row>
           </v-form>
         </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn text @click="cancel()">Cancel</v-btn>
-          <v-btn color="success darken-1" text @click="postRegistrationForm()">SEND</v-btn>
+        <v-card-actions class="justify-center pb-8">
+          <v-btn class="BC-Gov-PrimaryButton light" text @click="cancel()">Cancel</v-btn>
+          <v-btn class="BC-Gov-PrimaryButton" text @click="postRegistrationForm()">Submit</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
-
-    <!-- error dialog -->
-     <Dialog v-bind:show="errorOccurred">
+    <Dialog v-bind:show="errorOccurred">
       <template v-slot:icon>
         <v-icon large color="red">cancel</v-icon>
       </template>
@@ -78,8 +116,6 @@
         <p>Please include your Acronym as well as your IDIR username in your email.</p>
       </template>
     </Dialog>
-
-    <!-- success dialog -->
     <Dialog v-bind:show="registerSuccess">
       <template v-slot:icon>
         <v-icon large color="green">check_circle_outline</v-icon>
@@ -88,11 +124,8 @@
         <p>Your Registration was sent successfully. You will be sent an email to ${this.userInfo.emailAddress} when your application has been authorized.</p>
       </template>
     </Dialog>
-
   </v-btn>
 </template>
-
-
 <script>
 import ApiService from '@/common/apiService';
 import { FieldValidations } from '@/utils/constants.js';
