ef443d1c73dcd105fb97c03ba57429bb577b5c78
Jeremy Ho <jujaga@gmail.com>
2020-03-04 16:44:17 -0800

    Update backend to support /config endpoint for frontend use Update deployment config with updated probe path
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/app.js
+++ app/app.js
@@ -35,6 +35,16 @@ if (process.env.NODE_ENV !== 'test') {
 // Use Keycloak OIDC Middleware
 app.use(keycloak.middleware());
 
+// Frontend configuration endpoint
+apiRouter.use('/config', (_req, res, next) => {
+  try {
+    const frontend = config.get('frontend');
+    res.status(200).json(frontend);
+  } catch (err) {
+    next(err);
+  }
+});
+
 // GetOK Base API Directory
 apiRouter.get('/api', (_req, res) => {
   if (state.shutdown) {
@@ -45,12 +55,11 @@ apiRouter.get('/api', (_req, res) => {
 });
 
 // Host API endpoints
-const apiPath = '/api/v1';
-apiRouter.use(apiPath, v1Router);
-app.use('/', apiRouter);
+apiRouter.use(`${config.get('server.apiPath')}`, v1Router);
+app.use(`${config.get('server.basePath')}`, apiRouter);
 
 // Host the static frontend assets
-const staticFilesPath = '/app';
+const staticFilesPath = config.get('frontend.basePath');
 app.use(staticFilesPath, express.static(path.join(__dirname, 'frontend/dist')));
 
 // Handle 500
@@ -71,7 +80,7 @@ app.use((err, _req, res, _next) => {
 
 // Handle 404
 app.use((req, res) => {
-  if (req.originalUrl.startsWith(apiPath)) {
+  if (req.originalUrl.startsWith(`${config.get('server.basePath')}/api`)) {
     // Return a 404 problem if attempting to access API
     new Problem(404, 'Page Not Found', {
       detail: req.originalUrl
