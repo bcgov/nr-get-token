41279561d503410287a001fb59e667a5391222ea
Lucas O'Neil <lucasoneil@gmail.com>
2020-03-05 16:45:01 -0800

    test

--- app/frontend/src/components/HelloWorld.vue
+++ app/frontend/src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <v-container>
     <v-row class="text-center">
       <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">Welcome to Vuetify</h1>
+        <h1 class="display-2 font-weight-bold mb-3">Welcome to TEST</h1>
         <p class="subheading font-weight-regular">
           For help and collaboration with other Vuetify developers,
           <br />please join our online
