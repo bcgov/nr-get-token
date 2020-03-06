76873187d8895cfe229facac48c48cf8c347287f
Jeremy Ho <jujaga@gmail.com>
2020-03-06 14:46:22 -0800

    Attempt to extract out pipeline functions Reduce default patroni cpu limit to 500m
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- openshift/commonPipeline.groovy
+++ openshift/commonPipeline.groovy
@@ -1,6 +1,108 @@
 #!groovy
 import bcgov.GitHubHelper
 
+// ------------------
+// Pipeline Functions
+// ------------------
+
+// Parameterized deploy stage
+def deployStage(String stageEnv, String projectEnv, String hostEnv, String pathEnv) {
+  if (!stageEnv.equalsIgnoreCase('Dev')) {
+    input("Deploy to ${projectEnv}?")
+  }
+
+  notifyStageStatus("Deploy - ${stageEnv}", 'PENDING')
+
+  openshift.withCluster() {
+    openshift.withProject(projectEnv) {
+      if(DEBUG_OUTPUT.equalsIgnoreCase('true')) {
+        echo "DEBUG - Using project: ${openshift.project()}"
+      }
+
+      echo "Checking for ConfigMaps and Secrets in project ${openshift.project()}..."
+      if(!(openshift.selector('cm', "getok-frontend-config").exists() &&
+      openshift.selector('cm', "getok-oidc-config").exists() &&
+      openshift.selector('cm', "getok-sc-config").exists() &&
+      openshift.selector('cm', "getok-server-config").exists() &&
+      openshift.selector('secret', "getok-keycloak-secret").exists() &&
+      openshift.selector('secret', "getok-oidc-secret").exists() &&
+      openshift.selector('secret', "getok-sc-getokint-secret").exists() &&
+      openshift.selector('secret', "getok-sc-getoktest-secret").exists() &&
+      openshift.selector('secret', "getok-sc-getokprod-secret").exists() &&
+      openshift.selector('secret', "getok-sc-keycloakint-secret").exists() &&
+      openshift.selector('secret', "getok-sc-keycloaktest-secret").exists() &&
+      openshift.selector('secret', "getok-sc-keycloakprod-secret").exists() &&
+      openshift.selector('secret', "getok-sc-ches-secret").exists())) {
+        echo 'Some ConfigMaps and/or Secrets are missing. Please consult the openshift readme for details.'
+        throw new Exception('Missing ConfigMaps and/or Secrets')
+      }
+
+      if(openshift.selector('secret', "patroni-${JOB_NAME}-secret").exists()) {
+        echo "Patroni Secret already exists. Skipping..."
+      } else {
+        echo "Processing Patroni Secret..."
+        def dcPatroniSecretTemplate = openshift.process('-f',
+          'openshift/patroni.secret.yaml',
+          "APP_DB_NAME=${APP_NAME}",
+          "INSTANCE=${JOB_NAME}"
+        )
+
+        echo "Creating Patroni Secret..."
+        openshift.create(dcPatroniSecretTemplate)
+      }
+
+      // Apply Patroni Database
+      timeout(10) {
+        def dcPatroniTemplate
+        if(JOB_BASE_NAME.startsWith('PR-')) {
+          echo "Processing Patroni StatefulSet (Ephemeral)..."
+          dcPatroniTemplate = openshift.process('-f',
+            'openshift/patroni-ephemeral.dc.yaml',
+            "APP_NAME=${APP_NAME}",
+            "INSTANCE=${JOB_NAME}",
+            "NAMESPACE=${projectEnv}"
+          )
+        } else {
+          echo "Processing Patroni StatefulSet (Persistent)..."
+          dcPatroniTemplate = openshift.process('-f',
+            'openshift/patroni.dc.yaml',
+            "INSTANCE=${JOB_NAME}",
+            "NAMESPACE=${projectEnv}"
+          )
+        }
+
+        echo "Applying Patroni StatefulSet..."
+        def dcPatroni = openshift.apply(dcPatroniTemplate).narrow('statefulset')
+        dcPatroni.rollout().status('--watch=true')
+      }
+
+      commonPipeline.createDeploymentStatus(projectEnv, 'PENDING', JOB_NAME, hostEnv, pathEnv)
+
+      // Wait for deployments to roll out
+      timeout(10) {
+        // Apply App Server
+        echo "Tagging Image ${REPO_NAME}-app:${JOB_NAME}..."
+        openshift.tag("${TOOLS_PROJECT}/${REPO_NAME}-app:${JOB_NAME}", "${REPO_NAME}-app:${JOB_NAME}")
+
+        echo "Processing DeploymentConfig ${REPO_NAME}-app-${JOB_NAME}..."
+        def dcAppTemplate = openshift.process('-f',
+          'openshift/app.dc.yaml',
+          "REPO_NAME=${REPO_NAME}",
+          "JOB_NAME=${JOB_NAME}",
+          "NAMESPACE=${projectEnv}",
+          "APP_NAME=${APP_NAME}",
+          "ROUTE_HOST=${hostEnv}",
+          "ROUTE_PATH=${pathEnv}"
+        )
+
+        echo "Applying ${REPO_NAME}-app-${JOB_NAME} Deployment..."
+        def dcApp = openshift.apply(dcAppTemplate).narrow('dc')
+        dcApp.rollout().status('--watch=true')
+      }
+    }
+  }
+}
+
 // --------------------
 // Supporting Functions
 // --------------------
