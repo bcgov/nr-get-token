f799a2ba13e86d2307c941378819cdc6d62cede2
Jeremy Ho <jujaga@gmail.com>
2020-03-11 11:59:54 -0700

    Add user acronym get endpoint with some validation
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

---
+++ app/src/routes/v1/users.js
@@ -0,0 +1,30 @@
+const usersRouter = require('express').Router();
+const { param, validationResult } = require('express-validator');
+const Problem = require('api-problem');
+const validator = require('validator');
+
+const users = require('../../components/users');
+
+/** Returns acronyms associated with a user */
+usersRouter.get('/:keycloakId/acronyms', [
+  param('keycloakId', 'Must be a valid UUID')
+    .exists().custom(value => validator.isUUID(value))
+], async (req, res) => {
+  // Validate for Bad Requests
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return new Problem(422, {
+      detail: 'Validation failed',
+      errors: errors.array()
+    }).send(res);
+  }
+
+  const result = await users.getUserAcronyms(req.params.keycloakId);
+  if (result === null) {
+    return new Problem(404).send(res);
+  } else {
+    res.status(200).json(result);
+  }
+});
+
+module.exports = usersRouter;
