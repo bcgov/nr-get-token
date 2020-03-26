70b26b8efecfce8a0f28c5e1b9f77bdf2c3c5efc
Lucas O'Neil <lucasoneil@gmail.com>
2020-03-25 21:41:30 -0700

    Unneeded brackets

--- app/frontend/src/components/apiAccess/Step4.vue
+++ app/frontend/src/components/apiAccess/Step4.vue
@@ -6,17 +6,21 @@
     <br />
     <br />
     <div class="text-center">
-      <v-btn class="BC-Gov-PrimaryButton" text @click="finish()">Finish</v-btn>
+      <v-btn class="BC-Gov-PrimaryButton" text @click="finish">Finish</v-btn>
     </div>
   </v-container>
 </template>
 
 <script>
+import { mapMutations } from 'vuex';
+
 export default {
   name: 'ApiAccessStep4',
   methods: {
+    ...mapMutations('apiAccess', ['setStep']),
     finish() {
       this.$router.push({ name: 'MyApps' });
+      this.setStep(1);
     }
   }
 };
