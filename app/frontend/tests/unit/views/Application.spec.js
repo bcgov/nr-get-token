9cb55bdab57c322aa049f70c85ddbe867c510f64
Lucas O'Neil <lucasoneil@gmail.com>
2020-03-30 13:17:47 -0700

    Fix test

--- app/frontend/tests/unit/views/Application.spec.js
+++ app/frontend/tests/unit/views/Application.spec.js
@@ -1,5 +1,6 @@
 import { createLocalVue, shallowMount } from '@vue/test-utils';
 import Vuetify from 'vuetify';
+import Vuex from 'vuex';
 
 import router from '@/router';
 import Application from '@/views/Application.vue';
@@ -7,10 +8,19 @@ import Application from '@/views/Application.vue';
 const localVue = createLocalVue();
 localVue.use(router);
 localVue.use(Vuetify);
+localVue.use(Vuex);
+
 
 describe('Application.vue', () => {
+  let store;
+
+  beforeEach(() => {
+    store = new Vuex.Store();
+  });
+
   it('renders', () => {
     const wrapper = shallowMount(Application, {
+      store,
       localVue,
       stubs: ['ApplicationList', 'BaseSecure']
     });
