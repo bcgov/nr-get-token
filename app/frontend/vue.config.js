1a1e4d17ffea27b6faa237efda88372c44e16c53
Jeremy Ho <jujaga@gmail.com>
2020-05-07 09:44:07 -0700

    Change vue dev server to proxy all /api requests instead of just XHR ones
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/frontend/vue.config.js
+++ app/frontend/vue.config.js
@@ -1,9 +1,20 @@
+process.env.VUE_APP_VERSION = require('./package.json').version;
+
+const proxyConfig = {
+  target: 'http://localhost:8080',
+  ws: true,
+  changeOrigin: true
+};
+
 module.exports = {
   publicPath: './',
   'transpileDependencies': [
     'vuetify'
   ],
   devServer: {
-    proxy: 'http://localhost:8080'
+    proxy: {
+      '/config': proxyConfig,
+      '^/app/api': proxyConfig
+    }
   }
 };
