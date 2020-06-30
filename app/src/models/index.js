6b989f95d2e8fdee6bc3cbef44b42546cf6f358b
Jeremy Ho <jujaga@gmail.com>
2020-06-30 16:56:06 -0700

    Breaking change: Upgrade sequelize from 5.x to 6.x
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/src/models/index.js
+++ app/src/models/index.js
@@ -1,19 +1,21 @@
+'use strict';
+
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-
-const dbConfig = require(__dirname + '/../../config/database.js');
+const config = require(__dirname + '/../../config/database.js');
 const db = {};
 
-const sequelize = new Sequelize(dbConfig);
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
-fs.readdirSync(__dirname)
+fs
+  .readdirSync(__dirname)
   .filter(file => {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
