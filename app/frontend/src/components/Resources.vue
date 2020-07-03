c2426814a3fe619fd6145527cdeae45564161980
Csaky <Tim.Csaky@gov.bc.ca>
2020-07-03 13:15:07 -0700

    styling

--- app/frontend/src/components/Resources.vue
+++ app/frontend/src/components/Resources.vue
@@ -3,9 +3,8 @@
     <h1 class="text-center my-8">Common Services Documentation</h1>
     <v-card class="mx-auto mb-8" max-width="800">
       <v-toolbar flat color="grey lighten-3">
-        <v-toolbar-title class="px-3">Documentation Resources</v-toolbar-title>
+        <v-toolbar-title class="px-3">Resources</v-toolbar-title>
       </v-toolbar>
-
       <v-simple-table>
         <template>
           <tbody>
@@ -14,21 +13,46 @@
               <td>
                 <p>Use the Common Service Postman Collection to verify your service client credentials and see the APIs in action.</p>
                 <ul>
-                  <li class="my-2">Get your <a href="https://bcgov.github.io/common-service-showcase/#Onboarding">service client</a>.</li>
-                  <li> Download and install <a href="https://www.postman.com/">POSTMAN</a>.</li>
-                  <li class="my-2"> Download the <a href="https://bcgov.github.io/common-service-showcase/assets/files/common_services_postman_collection.json">collection</a>.</li>
-                  <li class="my-2"> Read the <a href="https://bcgov.github.io/common-service-showcase/assets/files/common_services_postman_readme.md">guide</a> to setup and run.</li>
+                  <li class="my-2">
+                    Get your
+                    <a
+                      href="https://bcgov.github.io/common-service-showcase/#Onboarding"
+                    >service client</a>.
+                  </li>
+                  <li>
+                    Download and install
+                    <a href="https://www.postman.com/">POSTMAN</a>.
+                  </li>
+                  <li class="my-2">
+                    Download the
+                    <a
+                      href="https://bcgov.github.io/common-service-showcase/assets/files/common_services_postman_collection.json"
+                    >collection</a>.
+                  </li>
+                  <li class="my-2">
+                    Read the
+                    <a
+                      href="https://bcgov.github.io/common-service-showcase/assets/files/common_services_postman_readme.md"
+                    >guide</a> to setup and run.
+                  </li>
                 </ul>
               </td>
             </tr>
-
             <tr>
               <td>API Documentation</td>
               <td>
                 <p>We do recommend that you read the API documentation before running the collection.</p>
                 <ul>
-                  <li class="my-2">Common Hosted Email Service (<a href="https://ches-master-9f0fbe-prod.pathfinder.gov.bc.ca/api/v1/docs">CHES</a>).</li>
-                  <li class="my-2">Common Document Generation Service (<a href="https://cdogs-master-idcqvl-prod.pathfinder.gov.bc.ca/api/v2/docs#tag/DocGen">CDOGS</a>).</li>
+                  <li class="my-2">
+                    Common Hosted Email Service (
+                    <a href="https://ches-master-9f0fbe-prod.pathfinder.gov.bc.ca/api/v1/docs">CHES</a>).
+                  </li>
+                  <li class="my-2">
+                    Common Document Generation Service (
+                    <a
+                      href="https://cdogs-master-idcqvl-prod.pathfinder.gov.bc.ca/api/v2/docs#tag/DocGen"
+                    >CDOGS</a>).
+                  </li>
                 </ul>
               </td>
             </tr>
@@ -38,36 +62,36 @@
               <td>
                 <ul>
                   <li class="my-2">
-                    <a href="https://github.com/bcgov/nr-get-token/wiki" target="_blank">https://github.com/bcgov/nr-get-token/wiki</a>
+                    <a
+                      href="https://github.com/bcgov/nr-get-token/wiki"
+                      target="_blank"
+                    >https://github.com/bcgov/nr-get-token/wiki</a>
                   </li>
                 </ul>
               </td>
             </tr>
-
           </tbody>
         </template>
       </v-simple-table>
-
     </v-card>
   </v-container>
 </template>
 <style lang="scss" scoped>
 @import '@/assets/scss/style.scss';
-td{
+td {
   padding: 2rem;
-  border: thin solid rgba(0, 0, 0, 0.12);
+  border-right: thin solid rgba(0, 0, 0, 0.12);
 }
-td:first-child{
+td:first-child {
   font-weight: 800;
   font-size: 120%;
 }
-
 @media #{map-get($display-breakpoints, 'sm-and-down')} {
   td {
     display: block;
     border: none !important;
   }
-  td:nth-child(2){
+  td:nth-child(2) {
     height: auto;
     border-bottom: thin solid rgba(0, 0, 0, 0.12) !important;
     padding: 1em 2em;
--- app/frontend/src/components/Welcome.vue
+++ app/frontend/src/components/Welcome.vue
@@ -3,20 +3,19 @@
     <h1 class="text-center my-8">GETOK Common Service Onboarding</h1>
     <v-card class="mx-auto mb-8" max-width="800">
       <v-toolbar flat color="grey lighten-3">
-        <v-toolbar-title class="px-3">How It Workss</v-toolbar-title>
+        <v-toolbar-title class="px-3">How It Works</v-toolbar-title>
       </v-toolbar>
-      <v-divider></v-divider>
       <div class="pa-6">
         <ul>
           <li class="my-2">
             <strong>New User:</strong> Go to
-            <router-link :to="{ name: 'RequestAccount' }">"Request Account"</router-link>and submit the form to add your application.
+            <router-link :to="{ name: 'RequestAccount' }">"Request Account"</router-link> and submit the form to add your application.
             <br />* Check your email for the confirmation, then you’re ready to go!
           </li>
           <li class="my-2">
             <strong>Existing User:</strong> Manage your application from
-            <router-link :to="{ name: 'MyApps' }">"My Applications"</router-link>or
-            <router-link :to="{ name: 'RequestAccount' }">"Request Account"</router-link>for a new application
+            <router-link :to="{ name: 'MyApps' }">"My Applications"</router-link> or
+            <router-link :to="{ name: 'RequestAccount' }">"Request Account"</router-link> for a new application
           </li>
           <li class="my-2">
             <strong>Get Token for your application:</strong>
@@ -29,7 +28,7 @@
 
     <v-card class="mx-auto mb-8" max-width="800">
       <v-toolbar flat color="grey lighten-3">
-        <v-toolbar-title class="px-3">Invite Your Team member</v-toolbar-title>
+        <v-toolbar-title class="px-3">Invite Your Team Members</v-toolbar-title>
       </v-toolbar>
       <v-divider></v-divider>
       <div class="pa-6">
@@ -66,7 +65,7 @@
         <ul>
           <li
             class="my-2"
-          >Please read the <router-link :to="{ name: 'Documentation' }">documenation</router-link> for Common Service APIs. You can easily test all the API calls with the <a href="https://bcgov.github.io/common-service-showcase/assets/files/common_services_postman_collection.json" target="_blank">POSTMAN collection</a>. We also have the <a href="https://github.com/bcgov/nr-get-token/wiki" target="_blank">Wiki</a> with useful resources for any team interested in using Common Services. To read more go to <router-link :to="{ name: 'Documentation' }">"Documentation"</router-link>
+          >Please read the <router-link :to="{ name: 'Documentation' }">documentation</router-link> for Common Service APIs. You can easily test all the API calls with the <a href="https://bcgov.github.io/common-service-showcase/assets/files/common_services_postman_collection.json" target="_blank">POSTMAN collection</a>. We also have the <a href="https://github.com/bcgov/nr-get-token/wiki" target="_blank">Wiki</a> with useful resources for any team interested in using Common Services. To read more go to <router-link :to="{ name: 'Documentation' }">"Documentation".</router-link>
           </li>
         </ul>
       </div>
