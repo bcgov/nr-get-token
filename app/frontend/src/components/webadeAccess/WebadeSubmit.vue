dadbbb72e5666ef2b17ee3bab222e526523a9884
Lucas O'Neil <lucasoneil@gmail.com>
2020-07-03 11:40:35 -0700

    Conf dialog

--- app/frontend/src/components/webadeAccess/WebadeSubmit.vue
+++ app/frontend/src/components/webadeAccess/WebadeSubmit.vue
@@ -1,37 +1,43 @@
 <template>
-  <v-dialog
-    v-model="confirmationDialog"
-    persistent
-    max-width="1200"
-    v-if="userAppCfg.deploymentMethod === 'deploymentDirect'"
-  >
-    <template v-slot:activator="{ on }">
-      <v-btn color="success" v-on="on" @click="getWebAdeConfig">Submit</v-btn>
-    </template>
-    <v-card>
-      <v-card-title class="headline">Are you sure?</v-card-title>
-      <v-card-text>
-        <p>
-          This will overwrite any existing WebADE configuration for the
-          <strong>{{ userAppCfg.applicationAcronym }}</strong> application
-          in the ISSS
-          <strong>{{ userAppCfg.clientEnvironment }}</strong> WebADE environment.
-        </p>
-        <p>The following shows the differences between the current configuration and the configuration that will be created. Are you sure you want to proceed?</p>
-        <v-progress-linear v-if="diffLoading" color="primary" indeterminate></v-progress-linear>
-        <pre id="webadeDiff"></pre>
-      </v-card-text>
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn text @click="confirmationDialog = false">CANCEL</v-btn>
-        <v-btn
-          color="green darken-1"
-          text
-          @click="confirmationDialog = false; submitConfig()"
-        >CONTINUE</v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
+  <div>
+    <v-dialog
+      v-model="confirmationDialog"
+      persistent
+      max-width="1200"
+      v-if="userAppCfg.deploymentMethod === 'deploymentDirect'"
+    >
+      <template v-slot:activator="{ on }">
+        <v-btn color="success" v-on="on" @click="getWebAdeConfig">Submit</v-btn>
+      </template>
+      <v-card>
+        <v-card-title class="headline">Are you sure?</v-card-title>
+        <v-card-text>
+          <p>
+            This will overwrite any existing WebADE configuration for the
+            <strong>{{ userAppCfg.applicationAcronym }}</strong> application
+            in the ISSS
+            <strong>{{ userAppCfg.clientEnvironment }}</strong> WebADE environment.
+          </p>
+          <p>The following shows the differences between the current configuration and the configuration that will be created. Are you sure you want to proceed?</p>
+          <v-progress-linear v-if="diffLoading" color="primary" indeterminate></v-progress-linear>
+          <pre id="webadeDiff"></pre>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn text @click="confirmationDialog = false">CANCEL</v-btn>
+          <v-btn
+            color="green darken-1"
+            text
+            @click="confirmationDialog = false; submitConfig()"
+          >CONTINUE</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog v-model="passwordDialog" persistent max-width="700">
+      <SuccessDetails/>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
@@ -42,6 +48,7 @@ export default {
   data() {
     return {
       confirmationDialog: false,
+      passwordDialog: false
     };
   },
   computed: {
@@ -79,6 +86,9 @@ export default {
       this.$store.commit('webadeAccess/clearConfigSubmissionMsgs');
 
       await this.$store.dispatch('webadeAccess/submitConfigForm');
+      if (this.configSubmissionSuccess) {
+        this.passwordDialog = true;
+      }
     }
   }
 };
